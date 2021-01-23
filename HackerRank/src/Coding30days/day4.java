603cc63e779c32cceada4c1d71dff90679ab066e
danielpark0 <37523983+danielpark0@users.noreply.github.com>
2021-01-23 21:44:10 +0900

    day4

---
+++ HackerRank/src/Coding30days/day4.java
@@ -0,0 +1,26 @@
+package Coding30days;
+
+import java.util.*;
+
+public class day4 {
+
+	private static final Scanner scanner = new Scanner(System.in);
+
+	public static void main(String[] args) {
+		int N = scanner.nextInt();
+		scanner.skip("(\r\n|[\n\r\u2028\u2029\u0085])?");
+		if(N%2==1){
+			System.out.println("Weird");
+		}
+		else{
+			if(N>=6&&N<=20){
+				System.out.println("Weird");
+			}
+			else{
+				System.out.println("Not Weird");
+			}
+		}
+		scanner.close();
+	}
+
+}
