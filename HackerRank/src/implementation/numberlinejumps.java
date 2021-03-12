50fe98e83cdbc13992ca1db9200a01930789acc2
danielpark0 <37523983+danielpark0@users.noreply.github.com>
2021-03-12 23:59:45 +0900

    numb

---
+++ HackerRank/src/implementation/numberlinejumps.java
@@ -0,0 +1,11 @@
+package implementation;
+
+public class numberlinejumps {
+	
+	// Complete the kangaroo function below.
+    static String kangaroo(int x1, int v1, int x2, int v2) {
+
+
+    }
+
+}
