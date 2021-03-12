e0cc01e5be28b1b8389255dfbf2d62eaa2b8eb5f
danielpark0 <37523983+danielpark0@users.noreply.github.com>
2021-03-13 00:38:14 +0900

    numberlinejumps

--- HackerRank/src/implementation/numberlinejumps.java
+++ HackerRank/src/implementation/numberlinejumps.java
@@ -3,9 +3,29 @@ package implementation;
 public class numberlinejumps {
 	
 	// Complete the kangaroo function below.
-    static String kangaroo(int x1, int v1, int x2, int v2) {
+	static String kangaroo(int x1, int v1, int x2, int v2) {
 
+        if(v1<=v2){
+            return "NO";
+        }
+        else{
+            while(true){
+                x1+=v1;
+                x2+=v2;
+                if(x1==x2){
+                    return "YES";
+                }
+                if(x2<x1){
+                    return "NO";
+                }
+            }
+        }
 
+    }
+    
+    public static void main (String [] args)
+    {
+    	
     }
 
 }
